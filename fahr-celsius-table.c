14696aac047bd132b1f0e66291ea97ee950071a4
hulatime <hulatime@tutanota.com>
2023-02-10 23:04:57 +0000

    exercise 1.3

--- fahr-celsius-table.c
+++ fahr-celsius-table.c
@@ -1,17 +1,25 @@
 #include "stdio.h"
 
+//int main() {
+//    int fahr, celsius;
+//    int lower, upper, step;
+//
+//    lower = 0;
+//    upper = 300;
+//    step = 20;
+//
+//    fahr = lower;
+//    while (fahr <= upper) {
+//        celsius = 5 * (fahr - 32) / 9;
+//        printf("%d\t%d\n", fahr, celsius);
+//        fahr = fahr + step;
+//    }
+//}
+
 int main() {
-    int fahr, celsius;
-    int lower, upper, step;
+    int fahr;
 
-    lower = 0;
-    upper = 300;
-    step = 20;
-
-    fahr = lower;
-    while (fahr <= upper) {
-        celsius = 5 * (fahr - 32) / 9;
-        printf("%d\t%d\n", fahr, celsius);
-        fahr = fahr + step;
+    for (fahr = 0; fahr <= 300; fahr = fahr + 20) {
+        printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
     }
 }
