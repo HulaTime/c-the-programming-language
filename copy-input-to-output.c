11c84185113c5410cad644635250bb8c6d85a8e0
max <max@jigsaw.xyz>
2023-02-12 18:46:00 +0000

    exercise 1.5 and copy input to output program

---
+++ copy-input-to-output.c
@@ -0,0 +1,21 @@
+//
+// Created by hulatime on 12/02/23.
+//
+#include <stdio.h>
+
+//int main() {
+//    int c;
+//    c = getchar();
+//    while (c != EOF) {
+//        putchar(c);
+//        c = getchar();
+//    }
+//}
+
+int main() {
+    int c;
+
+    while ((c = getchar()) != EOF) {
+        putchar(c);
+    }
+}
